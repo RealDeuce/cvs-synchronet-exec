78a8a946b619ba7db4783fccce9f20bab9c19c39
rswindell <rswindell>
2003-09-20 05:39:28 +0000

    Added support for -d command-line option to enable debug log output.

--- ircd.js
+++ ircd.js
@@ -1,4 +1,4 @@
-// $Id: ircd.js,v 1.37 2003/09/19 09:21:15 rswindell Exp $
+// $Id: ircd.js,v 1.38 2003/09/20 05:39:28 rswindell Exp $
 //
 // ircd.js
 //
@@ -23,7 +23,7 @@ load("sockdefs.js");
 load("nodedefs.js");
 
 // CVS revision
-const REVISION = "$Revision: 1.37 $".split(' ')[1];
+const REVISION = "$Revision: 1.38 $".split(' ')[1];
 // Please don't play with this, unless you're making custom hacks.
 // IF you're making a custom version, it'd be appreciated if you left the
 // version number alone, and add a token in the form of +hack (i.e. 1.0+cyan)
@@ -868,6 +868,9 @@ for (cmdarg=0;cmdarg<argc;cmdarg++) {
 			break;
 		case "-p":
 			cmdline_port = parseInt(argv[++cmdarg]);
+			break;
+		case "-d":
+			debug=true;
 			break;
 	}
 }
