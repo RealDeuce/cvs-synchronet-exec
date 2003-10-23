d8dd9680750688918cc8ee6536c97be160374939
rswindell <rswindell>
2003-10-23 00:10:29 +0000

    Converts header field names to lower case.

--- load/mailproc_util.js
+++ load/mailproc_util.js
@@ -2,7 +2,7 @@
 
 // Utility functions for Synchronet external mail processors
 
-// $Id: mailproc_util.js,v 1.2 2003/10/22 01:01:18 rswindell Exp $
+// $Id: mailproc_util.js,v 1.3 2003/10/23 00:10:29 rswindell Exp $
 
 // Parses raw RFC822-formatted messages for use with SMTP Mail Processors
 // Returns an array of header fields parsed from the msgtxt
@@ -17,7 +17,7 @@ function parse_msg_header(msgtxt)
 			break;
 		var match = msgtxt[i].match(/(\S+)\s*:\s*(.*)/);
 		if(match)
-			hdr[last_field=match[1]]=match[2];
+			hdr[last_field=match[1].toLowerCase()]=match[2];
 		else if(last_field)		// Folded header field
 			hdr[last_field]+=msgtxt[i];
 	}	
