5ad0e3ad25e21726c886b089268bd29a0f06b9b0
rswindell <rswindell>
2003-11-20 08:08:44 +0000

    Fixed undefined field (no properties) error for XHDR command.

--- nntpservice.js
+++ nntpservice.js
@@ -2,7 +2,7 @@
 
 // Synchronet Service for the Network News Transfer Protocol (RFC 977)
 
-// $Id: nntpservice.js,v 1.74 2003/11/14 22:17:28 rswindell Exp $
+// $Id: nntpservice.js,v 1.75 2003/11/20 08:08:44 rswindell Exp $
 
 // Example configuration (in ctrl/services.cfg):
 
@@ -14,7 +14,7 @@
 //					Xnews 5.04.25
 //					Mozilla 1.1 (Requires -auto, and a prior login via other method)
 
-const REVISION = "$Revision: 1.74 $".split(' ')[1];
+const REVISION = "$Revision: 1.75 $".split(' ')[1];
 
 var tearline = format("--- Synchronet %s%s-%s NNTP Service %s\r\n"
 					  ,system.version,system.revision,system.platform,REVISION);
@@ -394,6 +394,8 @@ while(client.socket.is_connected && !quit) {
 						field=hdr.ftn_reply;
 						break;
 				}
+				if(field==undefined)
+					field="";
 
 				writeln(format("%u %s",i,field.toString()));
 			}
