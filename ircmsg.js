c7d4c78fb4e985b9dce61e464ffed97d32cc5c0a
deuce <deuce>
2004-11-19 08:16:45 +0000

    Remove empty message detection (broken I guess) Expand tabs to spaces.

--- ircmsg.js
+++ ircmsg.js
@@ -1,10 +1,10 @@
 /* ircmsg.js */
 
-/* $Id: ircmsg.js,v 1.14 2004/11/19 08:09:08 deuce Exp $ */
+/* $Id: ircmsg.js,v 1.15 2004/11/19 08:16:45 deuce Exp $ */
 
 load("irclib.js");	// Thanks Cyan!
 
-const REVISION = "$Revision: 1.14 $".split(' ')[1];
+const REVISION = "$Revision: 1.15 $".split(' ')[1];
 
 var server="irc.synchro.net";
 var channel="#channel";
@@ -12,7 +12,6 @@ var port=6667;
 var nick="nick";
 var msg;
 var join=false;
-var passed_msg=0;
 
 for(i=0;i<argc;i++) {
 	switch(argv[i]) {
@@ -33,16 +32,14 @@ for(i=0;i<argc;i++) {
 			break;
 		case "-m":
 			msg=argv[++i];
-			passed_msg=1;
 			break;
 	}
 }
 
-if(passed_msg && (msg == undefined || msg.search(/^\s*$/))) {
-	log("Cowardly refusing to send empty message");
-	exit();
-}
-log("Message: "+msg);
+msg.replace(/\t/,
+	function(str,offset,s) {
+		return('        '.substr(0,(offset % 8)));
+	});
 
 log("Using nick: " + nick);
 log("Connecting to: " +server+ " port " + port);
