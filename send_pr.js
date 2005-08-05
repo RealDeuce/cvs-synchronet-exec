e49ac16317e230f81a929a3580fed71f7b0f1877
deuce <deuce>
2005-08-05 21:26:02 +0000

    Ensure user is still online during loops... ensure partial followups aren't submitted etc.
    Apparently, js/36 isn't really a bug, a script MUST check the online state
    itself.
    
    PR:36->closed

--- send_pr.js
+++ send_pr.js
@@ -1,9 +1,9 @@
-// $Id: send_pr.js,v 1.20 2005/08/05 17:52:09 deuce Exp $
+// $Id: send_pr.js,v 1.21 2005/08/05 21:26:02 deuce Exp $
 
 load("sbbsdefs.js");
 load("gnatslib.js");
 
-const REVISION = "$Revision: 1.20 $".split(' ')[1];
+const REVISION = "$Revision: 1.21 $".split(' ')[1];
 
 const MAX_LINE_LEN = 78
 
@@ -79,7 +79,7 @@ do {
 	if(console.aborted)
 		exit();
 	pr.Description += line + "\r\n";
-} while (line != '');
+} while (line != '' && bbs.online);
 console.print("\1y\1hSteps to reproduce the problem (Blank line ends):\r\n");
 pr.HowToRepeat = '';
 do {
@@ -87,7 +87,7 @@ do {
 	if(console.aborted)
 		exit();
 	pr.HowToRepeat += line + "\r\n";
-} while (line != '');
+} while (line != '' && bbs.online);
 console.print("\1y\1hFix/Workaround if known (Blank line ends):\r\n");
 pr.Fix = '';
 do {
@@ -95,7 +95,7 @@ do {
 	if(console.aborted)
 		exit();
 	pr.Fix += line + "\r\n";
-} while (line != '');
+} while (line != '' && bbs.online);
 
 var body='';
 body += "To: bugs\r\n";
@@ -120,11 +120,14 @@ body += ">Description:\r\n" + pr.Description;
 body += ">How-To-Repeat:\r\n" + pr.HowToRepeat;
 body += ">Fix:\r\n" + pr.Fix;
 
-if(!gnats.submit(body)) {
-	alert(gnats.error);
-	console.pause();
-} else {
-	console.print("\1y\1hProblem Report (PR) submitted successfully.\r\n");
-	console.print(gnats.message);
+// Only submit if the user is still online...
+if(bbs.online) {
+	if(!gnats.submit(body)) {
+		alert(gnats.error);
+		console.pause();
+	} else {
+		console.print("\1y\1hProblem Report (PR) submitted successfully.\r\n");
+		console.print(gnats.message);
+	}
 }
 gnats.close();
