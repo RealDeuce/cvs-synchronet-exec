4dcc7ce6b09a05eefbcfaeb032ef86dfc7caee0b
rswindell <rswindell>
2005-10-14 01:07:22 +0000

    Uses socket.option_list, if defined (v3.13b or later), otherwise, uses the sockopts array from sockdefs.js.

--- sockinfo.js
+++ sockinfo.js
@@ -1,10 +1,18 @@
-// $Id: sockinfo.js,v 1.1 2005/10/13 22:40:25 rswindell Exp $
+// $Id: sockinfo.js,v 1.2 2005/10/14 01:07:22 rswindell Exp $
 
+load("sockdefs.js");
+
+var socket;
 if(this.client)
 	socket=client.socket;
 else
-	socket=new Socket("test");
-printf("sendbuf = %d\r\n",socket.getoption("SNDBUF"));
-printf("recvbuf = %d\r\n",socket.getoption("RCVBUF"));
-printf("tcp_nodelay = %d\r\n",socket.getoption("TCP_NODELAY"));
-printf("keepalive = %d\r\n",socket.getoption("KEEPALIVE"));
+	socket=new Socket(SOCK_STREAM,"test");
+
+var option_list;
+if(socket.option_list)
+	option_list = socket.option_list; // dynamic list (v3.13b)
+else
+ 	option_list = sockopts; // static list (sockdefs.js)
+var opt;
+for(opt in option_list)
+	print(option_list[opt] +" = "+ socket.getoption(option_list[opt]));
