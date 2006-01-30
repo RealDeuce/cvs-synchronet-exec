407d5785c7bc82eb1227094665c1a7a6dee1a18a
deuce <deuce>
2006-01-30 05:50:26 +0000

    Add comment that K_GETSTR is a hack.

--- load/lightbar.js
+++ load/lightbar.js
@@ -1,6 +1,6 @@
 /*
  * Generic lightbar interface.
- * $Id: lightbar.js,v 1.18 2006/01/30 05:46:05 deuce Exp $
+ * $Id: lightbar.js,v 1.19 2006/01/30 05:50:26 deuce Exp $
  */
 
 /* ToDo: Support multiple columns */
@@ -341,6 +341,10 @@ function Lightbar_getval(current)
 		last_cur=this.current;
 
 		/* Get input */
+		/*
+		 * ToDo: K_GETSTR is to ensure that the users SPIN mode isn't used
+		 * This is a hack.
+		 */
 		var key=console.getkey(K_UPPER|K_GETSTR);
 		if(this.hotkeys.indexOf(key)!=-1)
 			return(key);
