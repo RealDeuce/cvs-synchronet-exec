5b87f55233f81e2b1e76761c95a889b958fb9fea
cyan <cyan>
2006-07-22 05:46:17 +0000

    * Optimize WHOWAS a little..

--- load/ircd_user.js
+++ load/ircd_user.js
@@ -1,4 +1,4 @@
-// $Id: ircd_user.js,v 1.22 2006/07/09 04:09:23 cyan Exp $
+// $Id: ircd_user.js,v 1.23 2006/07/22 05:46:17 cyan Exp $
 //
 // ircd_unreg.js
 //
@@ -21,7 +21,7 @@
 //
 
 ////////// Constants / Defines //////////
-const USER_REVISION = "$Revision: 1.22 $".split(' ')[1];
+const USER_REVISION = "$Revision: 1.23 $".split(' ')[1];
 
 const USERMODE_NONE			=(1<<0); // NONE
 const USERMODE_OPER			=(1<<1); // o
@@ -1348,10 +1348,10 @@ function User_Work() {
 		}
 		var firstnick="";
 		var aWhoWas;
+		var ww_nick_uc = cmd[1].toUpperCase();
 		for (aWhoWas=whowas_pointer;aWhoWas>=0;aWhoWas--) {
 			var wwh = WhoWasHistory[aWhoWas];
-			if (wwh && (wwh.nick.toUpperCase() ==
-			    cmd[1].toUpperCase())) {
+			if (wwh && (wwh.nick.toUpperCase() == ww_nick_uc)) {
 				this.numeric(314,wwh.nick + " " + wwh.uprefix + " " + wwh.host
 					+ " * :" + wwh.realname);
 				this.numeric(312,wwh.nick + " " + wwh.server + " :"
@@ -1362,8 +1362,7 @@ function User_Work() {
 		}
 		for (aWhoWas=whowas_buffer;aWhoWas>=whowas_pointer;aWhoWas--) {
 			var wwh = WhoWasHistory[aWhoWas];
-			if (wwh && (wwh.nick.toUpperCase() ==
-			    cmd[1].toUpperCase())) {
+			if (wwh && (wwh.nick.toUpperCase() == ww_nick_uc)) {
 				this.numeric(314,wwh.nick + " " + wwh.uprefix + " " + wwh.host
 					+ " * :" + wwh.realname);
 				this.numeric(312,wwh.nick + " " + wwh.server + " :"
