ef06822d5b684accb6b75e75f81654474d2b8ec1
deuce <deuce>
2006-08-22 17:46:59 +0000

    If this is a message, re-write editor.inf with the possibly changed subject. This doesn't currently actually accomplish anything, but a quick look shows that it *could* be done by having writemsg() re-read it and update the title as appropriate.  Currently, writemsg is always passed a modifyable title parameter with a max length as follows:
    bulkmail.cpp:  70
    email.cpp:     70
    fido.cpp:     127
    netmail.cpp:  255 (in ::inetmail())
    netmail.cpp:  127 (in ::qnetmail())
    postmsg.cpp:   70
    
    So, if editor.inf was re-read and, if the subject has changed, truncate it
    to 70 chars and place the result in the title parameter, it should Just Work.

--- fseditor.js
+++ fseditor.js
@@ -1,9 +1,9 @@
 /* ToDo: At what point should trailing whitespace be removed? */
-/* $Id: fseditor.js,v 1.53 2006/08/22 17:25:04 deuce Exp $ */
+/* $Id: fseditor.js,v 1.54 2006/08/22 17:46:59 deuce Exp $ */
 
 load("sbbsdefs.js");
 
-const REVISION = "$Revision: 1.53 $".split(' ')[1];
+const REVISION = "$Revision: 1.54 $".split(' ')[1];
 var line=new Array();
 var quote_line=new Array();
 var xpos=0;									/* Current xpos of insert point */
@@ -1800,14 +1800,15 @@ if(f.open("r",false)) {
 		quote_line=make_lines(quote_msg(word_wrap(f.read(),76)),'');
 	else
 		line=make_lines(word_wrap(f.read()),'');
+	f.close();
 }
 if(line.length==0)
 	line.push(new Line());
 drop_file = new File(system.node_dir + "editor.inf");
 if(drop_file.exists && drop_file.open("r")) {
 	info = drop_file.readAll();
-	delete drop_file;
-
+	drop_file.close();
+	file_remove(system.node_dir + "editor.inf");
 	subj=info[0];
 	to=info[1];
 	from=info[3];
@@ -1822,5 +1823,13 @@ if(subj=='') {
 	subj=subj.replace(/^.*[\\\/]/,'');
 }
 edit(use_quotes);
+if(edit_top==5) {
+	drop_file = new File(system.node_dir + "editor.inf");
+	if(drop_file.open("w")) {
+		info[0]=subj;
+		drop_file.writeln(info.join("\n"));
+		drop_file.close();
+	}
+}
 console.ctrlkey_passthru=oldpass;
 bbs.sys_status=old_status;
