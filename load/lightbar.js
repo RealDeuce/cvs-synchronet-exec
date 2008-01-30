ec7e3a388f2efdee66470f416bd50e9a4612b4a7
deuce <deuce>
2008-01-30 09:01:08 +0000

    Set the attribute back to the current attribute so that the cursor always remains visible even on terminals which draw the cursor using the background colour.

--- load/lightbar.js
+++ load/lightbar.js
@@ -1,6 +1,6 @@
 /*
  * Generic lightbar interface.
- * $Id: lightbar.js,v 1.24 2008/01/09 22:23:11 deuce Exp $
+ * $Id: lightbar.js,v 1.25 2008/01/30 09:01:08 deuce Exp $
  */
 
 /* ToDo: Support multiple columns */
@@ -353,6 +353,7 @@ function Lightbar_getval(current)
 			return(undefined);
 		}
 		console.gotoxy(cursx,cursy);
+		console.attributes=cattr;
 		if(ret!=undefined) {
 			this.nodraw=false;
 			return(ret);
@@ -700,4 +701,6 @@ function Lightbar_draw(current)
 	
 	// Move cursor to the current selection
 	console.gotoxy(cursx,cursy);
+	// Set colour so the cursor is visible
+	console.attributes=cattr;
 }
