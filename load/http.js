ac05fb85419d537752723c7271d3b2090d3db9a8
deuce <deuce>
2010-07-06 19:51:48 +0000

    Use request_path not path to include query data

--- load/http.js
+++ load/http.js
@@ -1,4 +1,4 @@
-/* $Id: http.js,v 1.5 2010/03/13 09:06:32 rswindell Exp $ */
+/* $Id: http.js,v 1.6 2010/07/06 19:51:48 deuce Exp $ */
 
 load('sockdefs.js');
 load("url.js");
@@ -36,7 +36,8 @@ function HTTPRequest()
 			throw("Unknown scheme! '"+this.url.scheme+"'");
 		if(this.url.path=='')
 			this.url.path='/';
-		this.request="GET "+this.url.path+" HTTP/1.0";
+		if(this.url.query != '');
+		this.request="GET "+this.url.request_path+" HTTP/1.0";
 		this.request_headers=[];
 		this.AddDefaultHeaders();
 	};
