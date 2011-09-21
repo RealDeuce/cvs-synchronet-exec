1618b1d30d3ac850c6b344b100c1c4c20bbc2d30
mcmlxxix <mcmlxxix>
2011-09-21 03:59:14 +0000

    fix bug in previous commit

--- load/json-db.js
+++ load/json-db.js
@@ -34,7 +34,7 @@
 */
 
 function JSONdb (fileName) {
-	this.VERSION = "$Revision: 1.10 $".replace(/\$/g,'').split(' ')[1];
+	this.VERSION = "$Revision: 1.11 $".replace(/\$/g,'').split(' ')[1];
 	
     /* database storage file */
 	if(fileName) 
@@ -494,8 +494,10 @@ function JSONdb (fileName) {
     
 	/* release any locks or subscriptions held by a disconnected client */
 	this.release = function(client_id) {
-		if(this.subscriptions[client_id]) 
+		if(this.subscriptions[client_id]) {
 			free_prisoner(client_id,this.shadow);
+			delete this.subscriptions[client_id];
+		}
 		for(var c=0;c<this.queue.length;c++) {
 			if(this.queue[c].client.id == client_id)
 				this.queue.splice(c--,1);
