cdd8dea9b859e6c61062a4250d745bda134eea20
mcmlxxix <mcmlxxix>
2011-09-27 04:05:50 +0000

    fix bug in previous commit

--- load/json-sock.js
+++ load/json-sock.js
@@ -1,6 +1,6 @@
 load("synchronet-json.js");
 /* socket mod version */
-Socket.prototype.VERSION = "$Revision: 1.3 $".replace(/\$/g,'').split(' ')[1];
+Socket.prototype.VERSION = "$Revision: 1.4 $".replace(/\$/g,'').split(' ')[1];
 /* round trip packet time */
 Socket.prototype.latency = 0;
 /* one way (latency / 2) */
@@ -25,11 +25,11 @@ Socket.prototype.sendJSON = function(object) {
 
 /*  socket prototype to automatically decode JSON data */
 Socket.prototype.recvJSON = function() { 
-	var data=this.recvline(this.max_recv,this.recv_wait); 
-	if(data != null) {
+	var packet=this.recvline(this.max_recv,this.recv_wait); 
+	if(packet != null) {
 		log(LOG_DEBUG,"<--" + this.descriptor + ": " + data);
 		try {
-			packet=JSON.parse(data,this.reviver);
+			packet=JSON.parse(packet,this.reviver);
 		} 
 		catch(e) {
 			log(LOG_ERROR,e);
