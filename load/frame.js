96173e7aa7373655236a8f4297e175612e31ccb7
mcmlxxix <mcmlxxix>
2013-03-05 18:44:07 +0000

    fixed bug in frame.clear()... i hope

--- load/frame.js
+++ load/frame.js
@@ -1,4 +1,4 @@
-/* $Id: frame.js,v 1.50 2013/03/01 21:27:45 mcmlxxix Exp $ */
+/* $Id: frame.js,v 1.51 2013/03/05 18:44:07 mcmlxxix Exp $ */
 
 /**
  	Javascript Frame Library 					
@@ -129,7 +129,7 @@ function Frame(x,y,width,height,attr,parent) {
 		height:undefined,
 		attr:undefined,
 		display:undefined,
-		data:[[]],
+		data:[],
 		open:false,
 		ctrl_a:false,
 		curr_attr:undefined,
@@ -826,6 +826,8 @@ function Frame(x,y,width,height,attr,parent) {
 		if(attr == undefined)
 			attr = this.attr;
 		for(var y=0;y<properties.data.length;y++) {
+			if(!properties.data[y])
+				continue;
 			for(var x=0;x<properties.data[y].length;x++) {	
 				if(properties.data[y][x]) {
 					properties.data[y][x].ch = undefined;
