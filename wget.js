445acb29db2aa1392bc8279c14547f308b2cc288
art <art>
2013-05-09 08:49:22 +0000

    Support for second optional argument, specifying output file. Use backslash() for filename. More one-trick-ponies.

--- wget.js
+++ wget.js
@@ -1,9 +1,19 @@
-/* $Id: wget.js,v 1.2 2010/03/19 01:15:25 rswindell Exp $ */
+/* $Id: wget.js,v 1.3 2013/05/09 08:49:22 art Exp $ */
 
 load("http.js");
 
-var url = argv[0];
-var filename=js.startup_dir + '/' + file_getname(url);
+var url = argv[0];	// First argument is the URL (required).
+var outfile;		// Pass a second argument as the outfile (optional).
+var filename=backslash(js.startup_dir);
+
+if (argv[1]) {
+	outfile = argv[1];
+	filename += outfile;
+} else {
+	filename += file_getname(url);
+}
+
+print("Writing to file: " + filename);
 
 var file = new File(filename);
 
