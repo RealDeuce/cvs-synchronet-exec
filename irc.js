92428497ca402de53dabee5e6ee3332096d6cc9b
deuce <deuce>
2013-08-12 18:03:01 +0000

    Bah, make it a right and proper delete.

--- irc.js
+++ irc.js
@@ -3,14 +3,14 @@
 // Deuce's IRC client module for Synchronet
 // With the "Manny Mods".  :-)
 
-// $Id: irc.js,v 1.44 2013/08/12 17:59:29 deuce Exp $
+// $Id: irc.js,v 1.45 2013/08/12 18:03:01 deuce Exp $
 
 // disable auto-termination.
 var old_auto_terminate=js.auto_terminate;
 js.on_exit("js.auto_terminate=old_auto_terminate");
 js.auto_terminate=false;
 
-const REVISION = "$Revision: 1.44 $".split(' ')[1];
+const REVISION = "$Revision: 1.45 $".split(' ')[1];
 const SPACEx80 = "                                                                                ";
 const MAX_HIST = 50;
 
@@ -1348,6 +1348,11 @@ function Screen_handle_key(key)  {
 			this.update_input_line();
 			break;
 		case "\x7f":
+			if(this.input_buffer.length > this.input_pos) {
+				this.input_buffer=this.input_buffer.slice(0,this.input_pos)+this.input_buffer.slice(this.input_pos+1);
+				this.update_input_line();
+			}
+			break;
 		case "\x08":
 			if(this.input_pos > 0)  {
 				this.input_buffer=this.input_buffer.slice(0,this.input_pos-1)+this.input_buffer.slice(this.input_pos);
