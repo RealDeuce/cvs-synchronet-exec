74be90c40b1e6246834d803302174163b208e531
rswindell <rswindell>
2013-09-24 09:24:43 +0000

    Added comment with instructions about how to avoid over-writing the values of the argc and argv properties in the parent script by passing a scope object to load().

--- load/modopts.js
+++ load/modopts.js
@@ -1,10 +1,16 @@
 /* modopts.js */
 
-/* $Id: modopts.js,v 1.2 2011/07/02 03:32:19 rswindell Exp $ */
+/* $Id: modopts.js,v 1.3 2013/09/24 09:24:43 rswindell Exp $ */
 
 /* Load Synchronet JS Module Control/Enable options from ctrl/modopts.ini */
 /* Parse a single .ini section using the argument (to load) as the section name */
 /* and return an object containing the key=value pairs as properties */
+
+/* To avoid over-writing the parent script's "argc" and "argv" values, 
+   pass a scope object to load(), like this:
+
+   options=load(new Object, "modopts.js", "your_module_name");
+*/
 
 
 function get_mod_options(modname)
