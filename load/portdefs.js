69b758d4fe729eab13e5976bdf5ab74a8d97cef3
rswindell <rswindell>
2015-09-09 01:43:52 +0000

    Added hotline and json (inter-bbs) TCP ports (5500 and 10088).

--- load/portdefs.js
+++ load/portdefs.js
@@ -1,4 +1,4 @@
-// $Id: portdefs.js,v 1.2 2015/09/04 01:36:49 rswindell Exp $
+// $Id: portdefs.js,v 1.3 2015/09/09 01:43:52 rswindell Exp $
 
 // Standard Unix service TCP and UDP port numbers
 
@@ -39,6 +39,8 @@ standard_service_port = {
 	"imaps":	993,
 	"ircs":		994,
 	"pop3s":	995,
+	"hotline":	5500,
 	"irc":		6667,
 	"http-alt":	8080,
+	"json":		10088,
 };
