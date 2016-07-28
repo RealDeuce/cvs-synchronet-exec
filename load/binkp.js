e04dc05c4bcb6d3406c0f7573e475d418039b191
deuce <deuce>
2016-07-28 10:58:21 +0000

    It appears that for incoming connections, binkp will NOT encrypt if there is no password... even if they requested encryption.

--- load/binkp.js
+++ load/binkp.js
@@ -417,7 +417,7 @@ BinkP.prototype.connect = function(addr, password, auth_cb, port, inet_host)
 	this.sendCmd(this.command.M_NUL, "LOC "+this.system_location);
 	this.sendCmd(this.command.M_NUL, "NDL "+this.capabilities);
 	this.sendCmd(this.command.M_NUL, "TIME "+new Date().toString());
-	this.sendCmd(this.command.M_NUL, "VER "+this.name_ver+",JSBinkP/"+("$Revision: 1.68 $".split(' ')[1])+'/'+system.platform+" binkp/1.1");
+	this.sendCmd(this.command.M_NUL, "VER "+this.name_ver+",JSBinkP/"+("$Revision: 1.69 $".split(' ')[1])+'/'+system.platform+" binkp/1.1");
 	this.sendCmd(this.command.M_ADR, this.addr_list.join(' '));
 
 	while(!js.terminated && this.remote_addrs === undefined) {
@@ -530,7 +530,7 @@ BinkP.prototype.accept = function(sock, auth_cb)
 	this.sendCmd(this.command.M_NUL, "LOC "+this.system_location);
 	this.sendCmd(this.command.M_NUL, "NDL 115200,TCP,BINKP");
 	this.sendCmd(this.command.M_NUL, "TIME "+new Date().toString());
-	this.sendCmd(this.command.M_NUL, "VER "+this.name_ver+",JSBinkP/"+("$Revision: 1.68 $".split(' ')[1])+'/'+system.platform+" binkp/1.1");
+	this.sendCmd(this.command.M_NUL, "VER "+this.name_ver+",JSBinkP/"+("$Revision: 1.69 $".split(' ')[1])+'/'+system.platform+" binkp/1.1");
 	this.sendCmd(this.command.M_ADR, this.addr_list.join(' '));
 
 	while(!js.terminated && this.authenticated === undefined) {
@@ -548,8 +548,14 @@ BinkP.prototype.accept = function(sock, auth_cb)
 					pwd = auth_cb(args, this);
 					if (pwd === undefined)
 						pwd = '-';
-					if (pwd === '-')
+					if (pwd === '-') {
 						this.authenticated = 'non-secure';
+						/*
+						 * It appears the binkd won't encrypt unless there's
+						 * a password, even if they requested CRYPT mode.
+						 */
+						this.will_crypt = false;
+					}
 					else
 						this.authenticated = 'secure';
 					this.sendCmd(this.command.M_OK, this.authenticated);
