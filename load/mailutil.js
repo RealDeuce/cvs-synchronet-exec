2ce76f13f6c38efa5e883b5800ad3f4914c585b2
rswindell <rswindell>
2016-08-12 22:15:02 +0000

    Added new function fidoaddr_to_emailaddr() which takes a message's From name (e.g. "Rob Swindell") and fidonet source address (e.g. "1:103/705") and converts it to an Internet email compatible address format (e.g. "Rob.Swindell@f705.n103.z1.fidonet") - for use with the listserver.js

--- load/mailutil.js
+++ load/mailutil.js
@@ -3,7 +3,7 @@
 // Parses Internet mail and USENET article header fields 
 // for use with newsutil.js and mailproc_util.js
 
-// $Id: mailutil.js,v 1.2 2005/03/04 21:20:56 rswindell Exp $
+// $Id: mailutil.js,v 1.3 2016/08/12 22:15:02 rswindell Exp $
 
 //Michael J. Ryan - 2004-04-16 - tracker1(at)theroughnecks.net
 // gets the name portion for the "to/from"
@@ -27,4 +27,32 @@ function mail_get_address(strIn) {
 	if (strIn.match(reEmail1)) return strIn.replace(reEmail1,"$1");
 	if (strIn.match(reEmail2)) return strIn.replace(reEmail2,"$1");
 	return null;
+}
+
+function fidoaddr_to_emailaddr(name, addr, tld)
+{
+	var ftn;
+
+	// Change "Joe Ray Schmoe" to "Joe.Ray.Schmoe"
+	name = name.replace(/\ /g, '.');
+
+	// If no top-level-domain specified, use "fidonet" by default
+	if(!tld) tld = "fidonet";
+
+	// FTN domain specified?
+	ftn = addr.match(/@([\w]+)$/);
+	if(ftn)
+		tld = ftn[1];
+
+	// Look for 4D addr
+	ftn = addr.match(/^([0-9]+)\:([0-9]+)\/([0-9]+)\.([0-9]+)/);
+	if(ftn)
+		return format("%s@p%u.f%u.n%u.z%u.%s", name, ftn[4], ftn[3], ftn[2], ftn[1], tld);
+
+	// Look for 3D addr
+	ftn = addr.match(/^([0-9]+)\:([0-9]+)\/([0-9]+)/);
+	if(ftn)
+		return format("%s@f%u.n%u.z%u.%s", name, ftn[3], ftn[2], ftn[1], tld);
+
+	return addr;
 }
