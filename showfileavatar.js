b6e51abdca37d73154f064ca6481fd38955c04ea
rswindell <rswindell>
2018-01-13 10:44:05 +0000

    avatar_lib.draw() will do the usernumber look-up for us now.

--- showfileavatar.js
+++ showfileavatar.js
@@ -1,4 +1,4 @@
-// $Id: showfileavatar.js,v 1.1 2018/01/12 22:20:25 rswindell Exp $
+// $Id: showfileavatar.js,v 1.2 2018/01/13 10:44:05 rswindell Exp $
 
 const FM_ANON			=(1<<1);
 const USER_ANSI         =(1<<1);
@@ -6,6 +6,6 @@ const USER_ANSI         =(1<<1);
 // Avatar support here:
 if(!(bbs.file_attr&FM_ANON) && console.term_supports(USER_ANSI)) {
 	var Avatar = load({}, 'avatar_lib.js');
-	Avatar.draw(system.matchuser(bbs.file_uploader), null, null, /* above: */true, /* right-justified: */true);
+	Avatar.draw(null, bbs.file_uploader, null, /* above: */true, /* right-justified: */true);
 	console.attributes = 0;	// Clear the background attribute as the next line might scroll, filling with BG attribute
 }
