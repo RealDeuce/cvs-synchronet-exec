b28cdaceb9bffea12ff421fa898e6b38cc0509d9
rswindell <rswindell>
2018-09-08 19:37:54 +0000

    Fix previous commit: String.slice(0, -8) is not the same thing as String.slice(-8).

--- binkit.js
+++ binkit.js
@@ -1,4 +1,4 @@
-// $Id: binkit.js,v 2.9 2018/09/08 00:35:14 rswindell Exp $
+// $Id: binkit.js,v 2.10 2018/09/08 19:37:54 rswindell Exp $
 
 /*
  * Intentionally simple "Advanced BinkleyTerm Style Outbound"
@@ -22,7 +22,7 @@ load('fidocfg.js');
 load('binkp.js');
 load('freqit_common.js');
 
-var REVISION = "$Revision: 2.9 $".split(' ')[1];
+var REVISION = "$Revision: 2.10 $".split(' ')[1];
 var version_notice = "BinkIT/" + REVISION;
 var semaphores = [];
 
@@ -86,7 +86,7 @@ function lock_flow(file)
 		return true;
 	}
 	log(LOG_DEBUG, "Locking "+ret.bsy.name);
-	if(!mkpath(ret.bsy.name.slice(-file_getname(ret.bsy.name).length)))
+	if(!mkpath(ret.bsy.name.slice(0, -file_getname(ret.bsy.name).length)))
 		log(LOG_WARNING, "MKPATH ERROR " + errno + " (" + errno_str + "): " + ret.bsy.name);
 	if (!ret.bsy.open("wb")) {	// Used to include 'e' mode flag (which never worked)
 		log(LOG_WARNING, "Error " + ret.bsy.error + " creating " + ret.bsy.name);
