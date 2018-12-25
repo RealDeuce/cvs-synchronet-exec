0d9f2d3e00c128f1461d5b4f9f9d9d51da6c526b
rswindell <rswindell>
2018-12-25 02:29:24 +0000

    Added xtrn_area.event[].settings bit-flag values (EVENT_*) froms sbbsdefs.h

--- load/sbbsdefs.js
+++ load/sbbsdefs.js
@@ -1,6 +1,6 @@
 /* Synchronet Object Model constants definitions - (mostly bit-fields) */
 
-/* $Id: sbbsdefs.js,v 1.80 2018/10/22 05:25:37 rswindell Exp $ */
+/* $Id: sbbsdefs.js,v 1.81 2018/12/25 02:29:24 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -517,7 +517,16 @@ var   EVENT_POST	=5;			/* Execute after message posted				*/
 var   EVENT_UPLOAD	=6;			/* Execute after file uploaded				*/
 var   EVENT_DOWNLOAD=7;			/* Execute after file downloaded			*/
 					    		/********************************************/
-
+								
+								/********************************************/
+								/* Bits for xtrn_area.event[].settings		*/
+								/********************************************/
+var EVENT_EXCL		(1<<0) 		/* Exclusive execution required				*/
+var EVENT_FORCE		(1<<1) 		/* Force users off-line for event			*/
+var EVENT_INIT		(1<<2)		/* Always run event after BBS init/re-init	*/
+var EVENT_DISABLED	(1<<3)		/* Disabled									*/
+								/********************************************/
+								
 					    		/********************************************/
 								/* Bits in mode of bbs.telnet_gate()		*/
 					    		/********************************************/
