cefe205131cce4ad82b6b2aa587283fd1a177564
rswindell <rswindell>
2018-12-30 08:12:30 +0000

    Added the LM_REVERSE load/read_mail() mode flag definition (from sbbsdef.h).

--- load/sbbsdefs.js
+++ load/sbbsdefs.js
@@ -1,6 +1,6 @@
 /* Synchronet Object Model constants definitions - (mostly bit-fields) */
 
-/* $Id: sbbsdefs.js,v 1.82 2018/12/25 02:30:24 rswindell Exp $ */
+/* $Id: sbbsdefs.js,v 1.83 2018/12/30 08:12:30 rswindell Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -648,10 +648,11 @@ var GLOB_PERIOD     =(1<<7); 	/* Leading `.' can be matched by metachars.  */
 var GLOB_ONLYDIR    =(1<<13);	/* Match only directories.  */
 
 								/* Bits in the lm_mode of bbs.read_mail()	*/
-var LM_UNREAD		=(1<<0)		/* Include un-read mail only				*/
-var LM_INCDEL		=(1<<1)		/* Include deleted mail		 				*/
-var LM_NOSPAM		=(1<<2)		/* Exclude SPAM								*/
-var LM_SPAMONLY		=(1<<3)		/* Load SPAM only							*/
+var LM_UNREAD		=(1<<0);	/* Include un-read mail only				*/
+var LM_INCDEL		=(1<<1);	/* Include deleted mail		 				*/
+var LM_NOSPAM		=(1<<2);	/* Exclude SPAM								*/
+var LM_SPAMONLY		=(1<<3);	/* Load SPAM only							*/
+var LM_REVERSE		=(1<<4);	/* Reverse the index order (newest-first)	*/
 
 								/********************************************/
 								/* Values for which in bbs.read_mail()		*/
