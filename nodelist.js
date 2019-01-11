d74e949c650d4453593177558d4b97239a7573ed
rswindell <rswindell>
2019-01-11 09:34:48 +0000

    Read the default web_inactivity value from modopts.ini [web] inactivity value. - this change is dependant on the updated load/modopts.js. Cache the presence_lib and nodelist options in the bbs.mods object to speed up the re-loading of this script from the BBS. This means that changes to the presence_lib or modopts.ini will require the user log-off/back-on to experience the changes (only for the nodelist module).

--- nodelist.js
+++ nodelist.js
@@ -1,23 +1,18 @@
-// $Id: nodelist.js,v 1.8 2019/01/11 04:53:14 rswindell Exp $
+// $Id: nodelist.js,v 1.9 2019/01/11 09:34:48 rswindell Exp $
 
 // Node Listing / Who's Online display script, replaces the bbs.whos_online() function
 // Installable as a Ctrl-U key handler ('jsexec nodelist install') or as an
 // 'exec/node list' replacement ('jexec nodelist').
 //
-// Command-line / load() options supported:
+// Command-line / load() arguments supported:
 // -active      Include active users/nodes only
 // -noself      Exclude current/own node from list/output
 // -noweb       Exclude web users from list/output
 
-// The modopts.ini [nodelist] settings are only used when executing from the BBS/terminal server
-// (e.g. not when executed via JSexec)
+// The modopts.ini [nodelist] and [web] settings are only used when executing
+// from the BBS/terminal server (e.g. not when executed via JSexec)
 
 "use strict";
-
-if(js.global.bbs)
-	require("text.js", 'NodeLstHdr');
-
-var presence = load({}, "presence_lib.js");
 
 if(argv.indexOf("install") >= 0) {
 	var cnflib = load({}, "cnflib.js");
@@ -26,7 +21,7 @@ if(argv.indexOf("install") >= 0) {
 		alert("Failed to read xtrn.cnf");
 		exit(-1);
 	}
-	xtrn_cnf.hotkey.push({ key: /* Ctrl-U */21, cmd: '?nodelist.js -active' });
+	xtrn_cnf.hotkey.push({ key: ascii(ctrl('U')), cmd: '?nodelist.js -active' });
 	
 	if(!cnflib.write("xtrn.cnf", undefined, xtrn_cnf)) {
 		alert("Failed to write xtrn.cnf");
@@ -35,23 +30,41 @@ if(argv.indexOf("install") >= 0) {
 	exit(0);
 }
 
+// Load the user-presence library (if not already loaded)
+var presence;
+if(js.global.bbs) {
+	presence = bbs.mods.presence_lib;
+	require("text.js", 'NodeLstHdr');
+}
+
+if(!presence) {
+	presence = load({}, "presence_lib.js");
+	if(js.global.bbs)
+		bbs.mods.presence_lib = presence;	// cache the loaded-lib
+}
 
 writeln();
 if(js.global.bbs) {
-	var options = load({}, "modopts.js", "nodelist");
-	if(!options)
-		options = {};
-	if(!options.format)
-		options.format = "\x01n\x01h%3d  \x01n\x01g%s";
-	if(!options.username_prefix)
-		options.username_prefix = '\x01h';
-	if(!options.status_prefix)
-		options.status_prefix = '\x01n\x01g';	
-	if(!options.errors_prefix)
-		options.errors_prefix = '\x01h\x01r';
+	var options = bbs.mods.nodelist_options;
+	if(!bbs.mods.nodelist_options) {
+		options = load({}, "modopts.js", "nodelist");
+		if(!options)
+			options = {};
+		if(!options.format)
+			options.format = "\x01n\x01h%3d  \x01n\x01g%s";
+		if(!options.username_prefix)
+			options.username_prefix = '\x01h';
+		if(!options.status_prefix)
+			options.status_prefix = '\x01n\x01g';	
+		if(!options.errors_prefix)
+			options.errors_prefix = '\x01h\x01r';
+		if(!options.web_inactivity)
+			options.web_inactivity = load({}, "modopts.js", "web", "inactivity");
+		bbs.mods.nodelist_options = options;	// cache the options
+	}
 	console.print(bbs.text(NodeLstHdr));
 } else{ // e.g. invoked via JSexec
-	var REVISION = "$Revision: 1.8 $".split(' ')[1];
+	var REVISION = "$Revision: 1.9 $".split(' ')[1];
 	options = { 
 		format: "Node %2d: %s", 
 		include_age: true, 
