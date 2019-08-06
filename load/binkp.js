7d5d0eff83863bfda54cd2532ae08b4907ac736c
deuce <deuce>
2019-08-06 13:32:21 +0000

    Use ConnectedSocket class to support outgoing IPv6 connections.

--- load/binkp.js
+++ load/binkp.js
@@ -1,4 +1,4 @@
-// $Id: binkp.js,v 1.120 2019/06/17 05:21:25 rswindell Exp $
+// $Id: binkp.js,v 1.121 2019/08/06 13:32:21 deuce Exp $
 
 require('sockdefs.js', 'SOCK_STREAM');
 require('fido.js', 'FIDO');
@@ -56,7 +56,7 @@ function BinkP(name_ver, inbound, rx_callback, tx_callback)
 	if (name_ver === undefined)
 		name_ver = 'UnknownScript/0.0';
 	this.name_ver = name_ver;
-	this.revision = "JSBinkP/" + "$Revision: 1.120 $".split(' ')[1];
+	this.revision = "JSBinkP/" + "$Revision: 1.121 $".split(' ')[1];
 	this.full_ver = name_ver + "," + this.revision + ',sbbs' + system.version + system.revision + '/' + system.platform;
 
 	if (inbound === undefined)
@@ -424,18 +424,35 @@ BinkP.prototype.connect = function(addr, password, auth_cb, port, inet_host)
 	if (inet_host === undefined)
 		inet_host = addr.inet_host;
 
-	if (this.sock === undefined)
-		this.sock = new Socket(SOCK_STREAM, "binkp");
-
 	log(LOG_INFO, format("Connecting to %s at %s:%u", addr, inet_host, port));
 	this.connect_host = inet_host;
 	this.connect_port = port;
-	if(!this.sock.connect(inet_host, port)) {
-		this.connect_error = this.sock.error;
-		this.sock = undefined;
-		log(LOG_WARNING, "Connection to "+inet_host+":"+port+" failed.");
-		return false;
+
+	if (js.global.ConnectedSocket != undefined) {
+		if (this.sock !== undefined)
+			this.sock.close();
+		try {
+			this.sock = new ConnectedSocket(inet_host, port, {protocol:'binkp'});
+		}
+		catch(e) {
+			log(LOG_WARNING, "Connection to "+inet_host+":"+port+" failed ("+e+").");
+			this.connect_error = e;
+			this.sock = undefined;
+			return false;
+		}
 	}
+	else {
+		if (this.sock === undefined)
+			this.sock = new Socket(SOCK_STREAM, "binkp");
+
+		if(!this.sock.connect(inet_host, port)) {
+			this.connect_error = this.sock.error;
+			this.sock = undefined;
+			log(LOG_WARNING, "Connection to "+inet_host+":"+port+" failed.");
+			return false;
+		}
+	}
+
 	log(LOG_DEBUG, "Connection to "+inet_host+":"+port+" successful");
 
 	this.authenticated = undefined;
