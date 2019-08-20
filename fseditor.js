574b16a9448266951897ab2ba17eff1a7ad68741
rswindell <rswindell>
2019-08-20 01:23:03 +0000

    Now that the CVS revision is 5 chars (1.10x), this messed up the status line (became a blank/solid blue line). Remove one space between "CTRL-K for help" and "Insert/Overwrite Mode" to accommodate the new longer revision. This should work for 898 more revisions. < 80 col terminals likely have a different issue wiht the status line we should look at as well.

--- fseditor.js
+++ fseditor.js
@@ -1,9 +1,9 @@
 /* ToDo: At what point should trailing whitespace be removed? */
-/* $Id: fseditor.js,v 1.100 2019/08/19 23:43:38 rswindell Exp $ */
+/* $Id: fseditor.js,v 1.101 2019/08/20 01:23:03 rswindell Exp $ */
 
 load("sbbsdefs.js");
 
-const REVISION = "$Revision: 1.100 $".split(' ')[1];
+const REVISION = "$Revision: 1.101 $".split(' ')[1];
 require("text.js", 'FileNotReceived');
 var line=new Array();
 var quote_line=new Array();
@@ -38,7 +38,7 @@ var tab_width=8;
 var hdr_fmt	= "\1b\1h%-4s\1n\1b: \1h\1c%.*s\1>\r\n";
 var hdr_field_width = console.screen_columns - 7;
 var stat_attr	= 0x1f;
-var stat_fmt	= "\1h\1w\0014 FSEditor v" + REVISION + " - Type \1yCTRL-K\1w for help          %s\1>\1n";
+var stat_fmt	= "\1h\1w\0014 FSEditor v" + REVISION + " - Type \1yCTRL-K\1w for help         %s\1>\1n";
 var subj,to,from;
 
 var options = load('modopts.js', 'fseditor');
