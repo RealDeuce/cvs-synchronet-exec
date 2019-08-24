69aa66d91a3926a1da982b9e2a1f4ae0d90eb277
echicken <echicken>
2019-08-24 04:26:40 +0000

    Try to load api_key from modopts.ini->[geoip], otherwise fall back on the baked-in one. Log a nag message that sysop should get an API key of their own if they don't done got one. Removed unusable if() block.

--- load/geoip.js
+++ load/geoip.js
@@ -1,9 +1,13 @@
-// $Id: geoip.js,v 1.11 2016/04/24 00:55:44 deuce Exp $
+// $Id: geoip.js,v 1.12 2019/08/24 04:26:40 echicken Exp $
 
 require('http.js', 'HTTPRequest');
+var settings = load('modopts.js', 'geoip');
 
-var geoipAPIKey='a1ddc4963461ca20bffd54bb926ce74dc1ecbb8a421122cdc3cdfef616f5aad1';	// Enter your API info here!
-
+if (settings && settings.api_key) {
+    var geoipAPIKey = settings.api_key;
+} else {
+    var geoipAPIKey='a1ddc4963461ca20bffd54bb926ce74dc1ecbb8a421122cdc3cdfef616f5aad1';	// Enter your API info here!
+}
 if(geoipAPIKey==undefined) {
 	log("You need to get a key from http://ipinfodb.com/register.php");
 	exit;
