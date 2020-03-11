a13e23730ee8106476fb37ceaab25a5ff4dd7502
rswindell <rswindell>
2020-03-11 17:23:11 +0000

    Create the inbound dir(s) before trying to move files into them

--- binkit.js
+++ binkit.js
@@ -1,4 +1,4 @@
-// $Id: binkit.js,v 2.28 2019/12/11 20:14:54 rswindell Exp $
+// $Id: binkit.js,v 2.29 2020/03/11 17:23:11 rswindell Exp $
 // vi: tabstop=4
 /*
  * Intentionally simple "Advanced BinkleyTerm Style Outbound"
@@ -22,7 +22,7 @@ load('fidocfg.js');
 load('binkp.js');
 load('freqit_common.js');
 
-var REVISION = "$Revision: 2.28 $".split(' ')[1];
+var REVISION = "$Revision: 2.29 $".split(' ')[1];
 var version_notice = "BinkIT/" + REVISION;
 var semaphores = [];
 // data/binkstats.ini
@@ -390,13 +390,18 @@ function handle_freq(reqfname, bp)
 	}
 }
 
-function rename_or_move(src, dst)
+function rename_or_move(src, dst_dir, dst_fname)
 {
 	var sf;
 	var df;
 	var buf;
 	var remain;
 
+	if (!mkpath(dst_dir)) {
+		log(LOG_ERR, "Error " + errno + " making directory: " + dst_dir);
+		return false;
+	}
+	var dst = dst_dir + dst_fname;
 	if (file_rename(src, dst))
 		return true;
 	sf = new File(src);
@@ -463,7 +468,7 @@ function rx_callback(fname, bp)
 				log(LOG_ERROR, "No secure inbound configured in sbbsecho!  Leaving secure file as '"+fname+"'.");
 			else {
 				log(LOG_INFO, "Moving '"+fname+"' to '"+secure_inbound+file_getname(fname)+"'.");
-				if (!rename_or_move(fname, secure_inbound+file_getname(fname)))
+				if (!rename_or_move(fname, secure_inbound, file_getname(fname)))
 					return false;
 			}
 		}
@@ -473,7 +478,7 @@ function rx_callback(fname, bp)
 				log(LOG_ERROR, "No inbound configured in sbbsecho!  Leaving insecure file as '"+fname+"'.");
 			else {
 				log(LOG_INFO, "Moving '"+fname+"' to '"+inbound+file_getname(fname)+"'.");
-				if (!rename_or_move(fname, inbound+file_getname(fname)))
+				if (!rename_or_move(fname, inbound, file_getname(fname)))
 					return false;
 			}
 		}
