1a0c06e384277fe872a46005ba948bb3e4d8e587
rswindell <rswindell>
2020-04-11 03:20:53 +0000

    Move the Unexpected data warning thing so it only happens if there was a command to send.

--- load/ftp.js
+++ load/ftp.js
@@ -1,4 +1,4 @@
-/* $Id: ftp.js,v 1.18 2020/04/10 07:11:19 deuce Exp $ */
+/* $Id: ftp.js,v 1.19 2020/04/11 03:20:53 rswindell Exp $ */
 
 require('sockdefs.js', 'SOCK_STREAM');
 
@@ -333,12 +333,11 @@ FTP.prototype.cmd = function(cmd, needresp)
 	if (!this.socket.is_connected)
 		throw("Socket disconnected");
 
-	while (this.socket.data_waiting) {
-		start = time();
-		log(LOG_ERROR, "Error: Unexpected data on control connection: "+this.socket.recvline(this.maxline, this.timeout - (time() - start)));
-	}
-
 	if (cmd !== undefined) {
+		while (this.socket.data_waiting) {
+			start = time();
+			log(LOG_ERROR, "Error: Unexpected data on control connection: "+this.socket.recvline(this.maxline, this.timeout - (time() - start)));
+		}
 		cmdline = cmd + '\r\n';
 		log(LOG_DEBUG, "CMD: '"+cmd.replace(/\xff/g, "\xff\xff")+"'");
 		if (this.socket.send(cmdline) != cmdline.length)
