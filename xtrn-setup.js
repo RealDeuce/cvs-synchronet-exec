4c287905ba758115d20b6e909fb974a0de27241a
rswindell <rswindell>
2020-04-27 02:10:25 +0000

    Go back to the simpler design, no more cleartoeol() calls, now that we have frame.erase() - thanks Matt!

--- xtrn-setup.js
+++ xtrn-setup.js
@@ -1,4 +1,4 @@
-// $Id: xtrn-setup.js,v 1.7 2020/04/25 09:22:01 rswindell Exp $
+// $Id: xtrn-setup.js,v 1.8 2020/04/27 02:10:25 rswindell Exp $
 // vi: tabstop=4
 
 load('sbbsdefs.js');
@@ -63,53 +63,31 @@ frame.cycle();
 
 var key;
 var xtrn;
-var high_water = 0;
 console.ungetstr(KEY_UP);
 while (!js.terminated) {
     key = console.getkey();
     if (key.toLowerCase() == 'q') break;
     tree.getcmd(key);
-    function crlf() {
-		info_frame.cleartoeol();
-		info_frame.crlf();
-    }
     if (key == KEY_UP || key == KEY_DOWN || key == KEY_HOME || key == KEY_END) {
         xtrn = tree.currentItem.__xtrn_setup;
-        info_frame.home();
-        info_frame.putmsg('\x01h\x01w' + xtrn.Name);
-		crlf();
+        info_frame.erase(' ');
+        info_frame.putmsg('\x01h\x01w' + xtrn.Name + '\r\n');
         if (xtrn.Desc) {
-			info_frame.cleartoeol();
-			info_frame.putmsg('\x01n\x01w' + xtrn.Desc);
-			crlf();
+			info_frame.putmsg('\x01n\x01w' + xtrn.Desc + '\r\n');
 		}
-		crlf();
+		info_frame.crlf();
         if (xtrn.By) {
-			info_frame.putmsg('\x01h\x01cBy\x01w:');
-			crlf();
-			info_frame.putmsg('\x01w' + xtrn.By);
-			crlf();
-			crlf();
+			info_frame.putmsg('\x01h\x01cBy\x01w:\r\n');
+			info_frame.putmsg('\x01w' + xtrn.By + '\r\n\r\n');
 		}
         if (xtrn.Cats) {
-			info_frame.putmsg('\x01h\x01cCategories\x01w:');
-			crlf();
-			info_frame.putmsg('\x01n' + xtrn.Cats);
-			crlf();
-			crlf();
+			info_frame.putmsg('\x01h\x01cCategories\x01w:\r\n');
+			info_frame.putmsg('\x01n' + xtrn.Cats + '\r\n\r\n');
 		}
         if (xtrn.Subs) {
-			info_frame.putmsg('\x01h\x01cSubcategories\x01w:');
-			crlf();
-			info_frame.putmsg('\x01n' + xtrn.Subs);
-			crlf();
+			info_frame.putmsg('\x01h\x01cSubcategories\x01w:\r\n');
+			info_frame.putmsg('\x01n' + xtrn.Subs + '\r\n');
 		}
-		var y = info_frame.cursor.y;
-		while(high_water > y) {
-			crlf();
-			high_water--;
-		}
-		high_water = y;
     }
     if (frame.cycle()) console.gotoxy(console.screen_columns, console.screen_rows);
 }
